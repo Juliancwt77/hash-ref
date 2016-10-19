306b3070ebdb5cfcedff072e20eafc6fa2f9a875
Prima Aulia Gusta <primaulia@gmail.com>
2016-10-19 14:14:04 +0800

    initial commit with bcrypt

---
+++ app.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/hash')
+
+var bcrypt = require('bcrypt')
+
+var userSchema = mongoose.Schema({
+  local: {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  }
+})
+
+userSchema.pre('save', function (next) {
+  console.log('before save hash the password')
+  console.log(this)
+
+  var user = this
+
+  bcrypt.genSalt(5, function (err, salt) {
+    if (err) return next(err)
+
+    bcrypt.hash(user.local.password, salt, function (err, hash) {
+      if (err) return next(err)
+
+      user.local.password = hash
+      console.log('after hash')
+      console.log(user)
+      next()
+    })
+  })
+})
+
+userSchema.post('save', function () {
+  // console.log('after the save, save successful')
+})
+
+var User = mongoose.model('User', userSchema)
+
+var newUser = new User({
+  local: {
+    email: 'primaulia@gmail.com',
+    password: 'test123'
+  }
+})
+
+newUser.save(function (err) {
+  if (err) console.log(err.message)
+  // console.log('new user saved')
+})
